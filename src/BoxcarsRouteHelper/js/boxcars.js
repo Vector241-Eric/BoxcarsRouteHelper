267480359aa80015c5c3baedc2165ee223bbc564
Eric Anderson <eric@vector241.com>
2015-08-09 22:55:03 -0500

    Can switch between pages.

---
+++ src/BoxcarsRouteHelper/js/boxcars.js
@@ -0,0 +1,23 @@
+﻿var Boxcars = Boxcars || {};
+
+(function () {
+    Boxcars.PageSwitchers = (function () {
+        var attachHandlers = function() {
+            $('#btn-routes').click(function() {
+                $('#destinations-container').hide();
+                $('#routes-container').show();
+            });
+            $('#btn-destinations').click(function() {
+                $('#routes-container').hide();
+                $('#destinations-container').show();
+            });
+        };
+        return {
+            attachHandlers: attachHandlers
+        }
+    })();
+})();
+
+$(function() {
+    Boxcars.PageSwitchers.attachHandlers();
+});
