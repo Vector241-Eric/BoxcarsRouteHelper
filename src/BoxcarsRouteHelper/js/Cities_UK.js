a1bf9453c0459cb1aa1e97af8074a73d9bafad78
Eric Anderson <eric@vector241.com>
2018-01-01 10:14:39 -0600

    Generate UK js files

---
+++ src/BoxcarsRouteHelper/js/Cities_UK.js
@@ -0,0 +1,29 @@
+//  WARNING: Generated Content
+//
+//  See the JavascriptDataGenerator project
+
+var Boxcars = Boxcars || {};
+Boxcars.Data = Boxcars.Data || {};
+
+Boxcars.Data.cities = [
+	'Aberdeen', 'Aberystwyth', 'Banbury', 'Bangor', 'Barnstaple', 
+	'Barrow-in-Furness', 'Bedford', 'Berwick', 'Birmingham', 'Blackburn', 
+	'Blaneau Festiniog', 'Boat of Garten', 'Boston', 'Bournemouth', 'Brecon', 
+	'Bridgwater', 'Brighton', 'Bristol', 'Carmarthen', 'Cambridge', 
+	'Canterbury', 'Cardiff', 'Carlisle', 'Chatham', 'Colchester', 
+	'Craven Arms', 'Crewe', 'Crianlarich', 'Darlington', 'Derby', 
+	'Doncaster', 'Dover', 'Dumfries', 'Dundee', 'Edinburgh', 
+	'Exeter', 'Fort William', 'Fraserburgh', 'Glasgow', 'Gloucester', 
+	'Grimsby', 'Guildford', 'Hamilton', 'Hastings', 'Hereford', 
+	'Hertford', 'Hull', 'Inverness', 'Ipswich', 'Keith', 
+	'Kilmarnock', 'Kings Lynn', 'Kyle of Lochalsh', 'Lancaster', 'Leeds', 
+	'Leicester', 'Lincoln', 'Liverpool', 'Llanidloes', 'London', 
+	'Mallaig', 'Manchester', 'Montrose', 'Newcastle', 'Neyland', 
+	'Norwich', 'Nottingham', 'Oban', 'Oxford', 'Padstow', 
+	'Peebles', 'Penzance', 'Perth', 'Peterborough', 'Plymouth', 
+	'Portsmouth', 'Reading', 'Rugby', 'Salisbury', 'Scarborough', 
+	'Sheffield', 'Shrewsbury', 'Southampton', 'Southend-on-Sea', 'Stafford', 
+	'Stirling', 'Stranraer', 'Stratford-on-Avon', 'Swansea', 'Swindon', 
+	'Tebay', 'Tunbridge Wells', 'Weymouth', 'Whitehaven', 'Wick', 
+	'Winchester', 'Worcester', 'Wrexham', 'Yarmouth', 'York', 
+];
---
+++ src/BoxcarsRouteHelper/js/Regions_UK.js
@@ -0,0 +1,30 @@
+//  WARNING: Generated Content
+//
+//  See the JavascriptDataGenerator project
+
+var Boxcars = Boxcars || {};
+Boxcars.Data = Boxcars.Data || {};
+
+Boxcars.Data.regions = {};
+Boxcars.Data.regions["ODD:2"] = "White Rose Country";
+Boxcars.Data.regions["ODD:3"] = "White Rose Country";
+Boxcars.Data.regions["ODD:4"] = "Scottish Lowlands";
+Boxcars.Data.regions["ODD:5"] = "Scottish Lowlands";
+Boxcars.Data.regions["ODD:6"] = "White Rose Country";
+Boxcars.Data.regions["ODD:7"] = "Midlands";
+Boxcars.Data.regions["ODD:8"] = "Red Rose Country";
+Boxcars.Data.regions["ODD:9"] = "Midlands";
+Boxcars.Data.regions["ODD:10"] = "Red Rose Country";
+Boxcars.Data.regions["ODD:11"] = "East Anglia";
+Boxcars.Data.regions["ODD:12"] = "Wales";
+Boxcars.Data.regions["EVEN:2"] = "West Country";
+Boxcars.Data.regions["EVEN:3"] = "West Country";
+Boxcars.Data.regions["EVEN:4"] = "Home Counties";
+Boxcars.Data.regions["EVEN:5"] = "West Country";
+Boxcars.Data.regions["EVEN:6"] = "East Anglia";
+Boxcars.Data.regions["EVEN:7"] = "Home Counties";
+Boxcars.Data.regions["EVEN:8"] = "Wales";
+Boxcars.Data.regions["EVEN:9"] = "London";
+Boxcars.Data.regions["EVEN:10"] = "Scottish Highlands";
+Boxcars.Data.regions["EVEN:11"] = "Scottish Highlands";
+Boxcars.Data.regions["EVEN:12"] = "Scottish Highlands";
